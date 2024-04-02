9c6f4015782e9313115a3c28f9b8189474e78a63
Shoxrux Marufjonov <shokha0502@gmail.com>
2024-04-02 19:24:53 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+let name = "Джон"
+let admin = name
+console.log(admin);
+
+/* Баланс пользователя */
+let balance = 25000
+
+//траты
+let car = 4480
+let food = 890
+
+//доходи 
+let invoice = 5500
+let stock = 4200
+
+//Итоги
+let totalBalance = balance - car - food + invoice + stock
+console.log(totalBalance);
+
+
+
+
